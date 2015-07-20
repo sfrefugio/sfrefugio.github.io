2a0101b9267497169b4d73757afc712f2fc3ac41
Scott <sfrefugio@gmail.com>
2015-07-20 13:15:27 -0400

    Initial custom

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+   // $(document).ready(function () {
+   //     var $scrollingDiv = $(".icon");
+
+   //     $(window).scroll(function () {
+   //         // $scrollingDiv.stop()
+   //         //     .animate({
+   //         //     "marginTop": ($(window).scrollTop() + 0) + "px"
+   //         // }, "slow");
+   //         $scrollingDiv.css("background-color", (($(window).scrollTop() / $(document).height()) > 0.15) ? "orange" : "");
+   //     });
+   // });
---
+++ video.js
@@ -0,0 +1,90 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+// //jQuery is required to run this code
+// $( document ).ready(function() {
+// 
+//     scaleVideoContainer();
+// 
+//     initBannerVideoSize('.video-container .poster img');
+//     initBannerVideoSize('.video-container .filter');
+//     initBannerVideoSize('.video-container video');
+// 
+//     $(window).on('resize', function() {
+//         scaleVideoContainer();
+//         scaleBannerVideoSize('.video-container .poster img');
+//         scaleBannerVideoSize('.video-container .filter');
+//         scaleBannerVideoSize('.video-container video');
+//     });
+// 
+// });
+// 
+// function scaleVideoContainer() {
+// 
+//     var height = $(window).height() + 5;
+//     var unitHeight = parseInt(height) + 'px';
+//     $('.homepage-hero-module').css('height',unitHeight);
+// 
+// }
+// 
+// function initBannerVideoSize(element){
+// 
+//     $(element).each(function(){
+//         $(this).data('height', $(this).height());
+//         $(this).data('width', $(this).width());
+//     });
+// 
+//     scaleBannerVideoSize(element);
+// 
+// }
+// 
+// function scaleBannerVideoSize(element){
+// 
+//     var windowWidth = $(window).width(),
+//     windowHeight = $(window).height() + 5,
+//     videoWidth,
+//     videoHeight;
+// 
+//     console.log(windowHeight);
+// 
+//     $(element).each(function(){
+//         var videoAspectRatio = $(this).data('height')/$(this).data('width');
+// 
+//         $(this).width(windowWidth);
+// 
+//         if(windowWidth < 1000){
+//             videoHeight = windowHeight;
+//             videoWidth = videoHeight / videoAspectRatio;
+//             $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+// 
+//             $(this).width(videoWidth).height(videoHeight);
+//         }
+// 
+//         $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+// 
+//     });
+// }
